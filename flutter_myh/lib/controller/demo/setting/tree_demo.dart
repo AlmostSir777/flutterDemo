5b54d77ab7a1106c8950cf089cb5699b1a3edad8
闵玉辉 <min.yuhui@ruanyun.net>
2020-12-25 17:57:15 +0800

    树形节点列表数据-待完善

--- flutter_myh/lib/controller/demo/setting/tree_demo.dart
+++ flutter_myh/lib/controller/demo/setting/tree_demo.dart
@@ -4,6 +4,8 @@ import '../demo_config.dart';
 import 'package:flutter/services.dart';
 import 'dart:convert';
 
+import '../../../custom/section_list_view.dart';
+
 class TreeDemo extends StatefulWidget {
   @override
   _TreeDemoState createState() => _TreeDemoState();
@@ -28,11 +30,125 @@ class _TreeDemoState extends State<TreeDemo> {
   Widget build(BuildContext context) {
     return BaseNormalContainer(
       title: '树形结构',
-      body: ListView.builder(
-        itemBuilder: (BuildContext context, int index) => NodeItemWidget(
-          node: _list[index],
-        ),
-        itemCount: _list.length,
+      // body: SingleChildScrollView(
+      //   child: ExpansionPanelList(
+      //     children: _list.map((node) => _buildExpansionPanel(node)).toList(),
+      //     expansionCallback: (int row, bool isExpand) {
+      //       NodeModel node = _list[row];
+      //       setState(() {
+      //         node.isOpen = !node.isOpen;
+      //       });
+      //     },
+      //   ),
+      // ),
+      body: _list.length == 0
+          ? Center(child: Text('加载种...'))
+          : SectionListView(
+              sectionCount: () => _list.length,
+              headerSectionComplete: (_, int section) {
+                NodeModel node = _list[section];
+              },
+              itemComplete: (_, IndexPath indexPath) {
+                return _buildSection(
+                    _list[indexPath.section].children[indexPath.row]);
+              },
+              itemsCount: (int section) =>
+                  _list[section].isOpen ? _list[section].children.length : 0,
+            ),
+    );
+  }
+
+  Widget _buildSection(NodeModel node) {}
+
+  ExpansionPanelList _buildExpansionPanelList(NodeModel value) {
+    return ExpansionPanelList(
+      children:
+          value.children.map((node) => _buildExpansionPanel(node)).toList(),
+      expansionCallback: (int row, bool isExpand) {
+        NodeModel node = value.children[row];
+        setState(() {
+          node.isOpen = !node.isOpen;
+        });
+      },
+    );
+  }
+
+  ExpansionPanel _buildExpansionPanel(NodeModel value) {
+    return ExpansionPanel(
+        isExpanded: value.isOpen,
+        headerBuilder: (BuildContext context, bool expand) {
+          return Container(
+            padding: EdgeInsets.only(
+              left: 10 + value.level * 8.0,
+            ),
+            alignment: Alignment.centerLeft,
+            child: Text(
+              value.nodeName,
+              style: TextStyle(
+                color: expand ? theme_color : text_color,
+                fontSize: value.level == 0 ? 16 : 14,
+              ),
+            ),
+          );
+        },
+        canTapOnHeader: value.children.isEmpty,
+        body: value.children.isEmpty
+            ? Container()
+            : _buildExpansionPanelList(value));
+  }
+}
+
+typedef void SelectBlock(NodeModel node);
+
+class NodeItemWidget extends StatelessWidget {
+  final NodeModel node;
+  final SelectBlock selectBlock;
+  NodeItemWidget({
+    this.node,
+    this.selectBlock,
+  });
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 60.0,
+      color: node.isSelect ? theme_color.withOpacity(0.3) : Colors.white,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Row(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              IconButton(
+                  icon:
+                      Icon(node.isOpen ? Icons.lock_open : Icons.lock_outline),
+                  onPressed: () {
+                    if (selectBlock != null) {
+                      selectBlock(node);
+                    }
+                  }),
+              SizedBox(
+                width: 10,
+              ),
+              Text(
+                node.nodeName,
+                style: TextStyle(color: theme_color, fontSize: 15),
+              ),
+            ],
+          ),
+          Container(
+            padding: EdgeInsets.only(right: 10),
+            alignment: Alignment.centerRight,
+            child: Text(
+              node.isSelect ? '选中' : '非选中',
+              style: TextStyle(
+                  color: node.isSelect
+                      ? theme_color
+                      : Colors.grey.withOpacity(0.4),
+                  fontSize: 15),
+            ),
+          ),
+        ],
       ),
     );
   }
@@ -46,6 +162,16 @@ class NodeModel {
   List nodeValue;
   String orderMeritName;
   int level;
+  bool isOpen;
+  bool isSelect;
+
+  static show(NodeModel node) {
+    while (node.children.isNotEmpty) {
+      node.isSelect = true;
+      node.children.forEach((obj) {});
+    }
+  }
+
   static List<NodeModel> jsonToData(List value) {
     List<NodeModel> datas = [];
     value.forEach((obj) {
@@ -62,7 +188,9 @@ class NodeModel {
       ..nodeValue = value['NodeValue']
       ..orderMeritName = value['OrderMeritName']
       ..children = []
-      ..level = level;
+      ..level = level
+      ..isOpen = false
+      ..isSelect = false;
     if (value['Children'] != null) {
       List children = value['Children'];
       children.forEach((obj) {
@@ -70,6 +198,22 @@ class NodeModel {
       });
     }
     return model;
+  }
+}
+
+class CustomNodeItemWidget extends StatefulWidget {
+  final NodeModel node;
+  CustomNodeItemWidget({
+    this.node,
+  });
+  @override
+  _CustomNodeItemWidgetState createState() => _CustomNodeItemWidgetState();
+}
+
+class _CustomNodeItemWidgetState extends State<CustomNodeItemWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return SectionListView(itemComplete: null, itemsCount: null);
   }
 }
 
@@ -87,14 +231,18 @@ class _NodeItemWidgetState extends State<NodeItemWidget> {
   Widget build(BuildContext context) {
     return ListTile(
       title: _buildItem(widget.node),
+      contentPadding: EdgeInsets.only(left: 0.0, right: 0.0),
+      selected: false,
     );
   }
 
   Widget _buildItem(NodeModel bean) {
     if (bean.children.isEmpty) {
       return ListTile(
+        contentPadding: EdgeInsets.only(left: 0.0, right: 0.0),
+        selected: false,
         title: Container(
-          padding: EdgeInsets.only(left: (10 + bean.level * 8).toDouble()),
+          padding: EdgeInsets.only(left: (20 + bean.level * 8).toDouble()),
           alignment: Alignment.centerLeft,
           child: Text(bean.nodeName),
         ),
@@ -105,7 +253,11 @@ class _NodeItemWidgetState extends State<NodeItemWidget> {
     }
     return ExpansionTile(
       key: PageStorageKey<NodeModel>(bean),
-      title: Text(bean.nodeName),
+      title: Container(
+        padding: EdgeInsets.only(left: (bean.level * 8).toDouble()),
+        alignment: Alignment.centerLeft,
+        child: Text(bean.nodeName),
+      ),
       children: bean.children.map<Widget>(_buildItem).toList(),
     );
   }
